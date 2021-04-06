e0d1b77aa100cc1a35885d706a5aaae15c709c03
tushargandre <53211946+tushargandre@users.noreply.github.com>
2021-04-06 15:23:49 +0530

    Update main.js

--- beatlancer/main.js
+++ beatlancer/main.js
@@ -12,16 +12,3 @@ function findPosition(obj) {
     } 
 } 
 
-function credits() { 
-    window.scroll(0,  
-findPosition(document.getElementById("credits"))); 
-} 
-function findPosition(obj) { 
-    var currenttop = 0; 
-    if (obj.offsetParent) { 
-        do { 
-            currenttop += obj.offsetTop; 
-        } while ((obj = obj.offsetParent)); 
-        return [currenttop]; 
-    } 
-} 
